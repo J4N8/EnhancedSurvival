da104fe2b3a52bc0fe325960e694937a29c10395
J4N8 <43821141+J4N8@users.noreply.github.com>
2021-06-15 18:07:42 +0200

    Initial commit

---
+++ src/main/java/j4n8/enhancedsurvival/EnhancedSurvival.java
@@ -0,0 +1,17 @@
+package j4n8.enhancedsurvival;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EnhancedSurvival extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
